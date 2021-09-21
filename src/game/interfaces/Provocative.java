b1de0ee651c843c29211eed037272e8b936e4067
Dylan Clements <dcle0002@student.monash.edu>
2021-09-21 22:01:26 +1000

    documentatino provocative

--- src/game/interfaces/Provocative.java
+++ src/game/interfaces/Provocative.java
@@ -2,9 +2,20 @@ package game.interfaces;
 
 import edu.monash.fit2099.engine.Actor;
 
+/**
+ * Interface for Actors that have the ability to be provoked by the Player, therefore switching behaviours
+ */
 public interface Provocative {
+    /**
+     * Method that will switch an actor's current behaviour to AggroBehaviour
+     * @param target actor who will be targeted with aggroBehaviour
+     */
     void switchAggroBehaviour(Actor target);
 
+    /**
+     * Grab the behaviour, default method.
+     * @return null, but override in implementation
+     */
     default Behaviour getBehaviour() {
         return null;
     }
