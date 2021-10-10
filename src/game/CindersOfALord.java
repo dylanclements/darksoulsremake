0cd43de0e96d847c9be4ea484abcd2125156003c
Dylan Clements <dcle0002@student.monash.edu>
2021-10-10 18:34:55 +1100

    added cinders of a lord

---
+++ src/game/CindersOfALord.java
@@ -0,0 +1,14 @@
+package game;
+
+public class CindersOfALord extends PortableItem {
+    /**
+     * Constructor.
+     *
+     * @param name        name of the item
+     * @param displayChar how this item is represented on the map.
+     */
+    public CindersOfALord(String name, char displayChar) {
+        super(name, displayChar);
+    }
+
+}
--- src/game/LordOfCinder.java
+++ src/game/LordOfCinder.java
@@ -3,6 +3,7 @@ package game;
 
 import edu.monash.fit2099.engine.Actor;
 
+
 /**
  * The boss of Design o' Souls
  */
@@ -10,13 +11,15 @@ public abstract class LordOfCinder extends Actor {
 
     /**
      * Lord of Cinder constructor.
+     * Also adds cinders to inventory
      * @param name name of the lord of cinder
      * @param displayChar how this lord of cinder is represented on the game map
      * @param hitPoints lord of cinder's health
      */
     public LordOfCinder(String name, char displayChar, int hitPoints) {
         super(name, displayChar, hitPoints);
+        String cindersName = name + "'s Cinders";
+        CindersOfALord cinders = new CindersOfALord(cindersName, 'L');
+        this.addItemToInventory(cinders);
     }
-
-    // TODO: Cinders of a lord abstract method here (optional though)
 }
