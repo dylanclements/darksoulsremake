dd1e61720998d2e3be5285304897178ebcf0e56b
Dylan Clements <dcle0002@student.monash.edu>
2021-10-10 21:12:43 +1100

    added maps class, refactored application

--- src/game/Application.java
+++ src/game/Application.java
@@ -1,8 +1,6 @@
 package game;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 import edu.monash.fit2099.engine.*;
@@ -12,6 +10,113 @@ import edu.monash.fit2099.engine.*;
  *
  */
 public class Application {
+	/**
+	 * Spawn the player
+	 * @param spawnLocation location the player will spawn in
+	 * @param world the world
+	 */
+	private static void spawnPlayer(Location spawnLocation, World world) {
+		Actor player = new Player("Unkindled (Player)", '@', 200, spawnLocation);
+		world.addPlayer(player, spawnLocation);
+	}
+
+	/**
+	 * Spawn a bonfire
+	 * @param location location the bonfire will exist in
+	 * @param bonfireName name of the bonfire
+	 */
+	private static void spawnBonfire(Location location, String bonfireName) {
+		location.setGround(new Bonfire(location, bonfireName));
+	}
+
+	/**
+	 * Spawn a Skeleton
+	 * @param location location the skeleton will be spawned on
+	 * @param skeletonName name of the skeleton
+	 */
+	private static void spawnSkeleton(Location location, String skeletonName) {
+		location.addActor(new Skeleton(skeletonName, location));
+	}
+
+	/**
+	 * Spawn a Yhorm
+	 * @param spawnLocation location yhorm will be spawned on
+	 */
+	private static void spawnYhorm(Location spawnLocation) {
+		spawnLocation.addActor(new Yhorm(spawnLocation));
+	}
+
+	/**
+	 * Spawn storm ruler
+	 * @param spawnLocation location that storm ruler will be spawned on
+	 */
+	private static void spawnStormRuler(Location spawnLocation) {
+		spawnLocation.addItem(new StormRuler());
+	}
+
+	/**
+	 * Spawn a fog door that leads to another map
+	 * @param mapASpawnLocation location on map A that the fog door will exist on
+	 * @param mapBSpawnLocation location on map B that the fog door will exist on
+	 * @param front location that grants the action
+	 * @param back other location that grants the action
+	 */
+	private static void spawnInterMapFogDoor(Location mapASpawnLocation, Location mapBSpawnLocation,
+											 Location front, Location back) {
+		assert front.map() != back.map();
+		FogDoor mapAFogDoor = new FogDoor(front, back);
+		mapASpawnLocation.setGround(mapAFogDoor);
+		FogDoor mapBFogDoor = new FogDoor(back, front);
+		mapBSpawnLocation.setGround(mapBFogDoor);
+	}
+
+	/**
+	 * Spawn a fog door that leads to the same map
+	 * @param spawnLocation location that the fog door will exist on
+	 * @param front location that grants the action
+	 * @param back other location that grants the action
+	 */
+	private static void spawnIntraMapFogDoor(Location spawnLocation, Location front, Location back) {
+		assert front.map() == back.map();
+		FogDoor fogDoor = new FogDoor(front, back);
+		spawnLocation.setGround(fogDoor);
+	}
+
+	/**
+	 * Spawn Aldrich
+	 * @param spawnLocation location that Aldrich will spawn on
+	 */
+	private static void spawnAldrich(Location spawnLocation) {
+		spawnLocation.addActor(new Aldrich(spawnLocation));
+	}
+
+	/**
+	 * Spawn random chests on the map.
+	 * @param map map that chests will spawn on
+	 */
+	private static void spawnRandomChests(GameMap map) {
+		// Spawn chests randomly in profane capital and anor londo
+		Random r = new Random();
+		float SPAWN_CHANCE = 0.005f;
+		NumberRange xRange = map.getXRange();
+		NumberRange yRange = map.getYRange();
+		ArrayList<Location> dirtLocations = new ArrayList<>();
+
+		for (int x = xRange.min(); x < xRange.max(); x++){
+			for (int y = yRange.min(); y < yRange.max(); y++){
+				if (map.at(x,y).getGround() instanceof Dirt){
+					dirtLocations.add(map.at(x,y));
+				}
+			}
+		}
+		for (Location dirtLocation : dirtLocations) {
+			if (r.nextFloat() <= SPAWN_CHANCE) {
+				Ground oldGround = dirtLocation.getGround();
+				dirtLocation.setGround(new Chest(oldGround, dirtLocation));
+			}
+		}
+
+	}
 
 	/**
 	 * Game starts from here.
@@ -24,184 +129,61 @@ public class Application {
 					new Dirt(), new Wall(), new Floor(), new Valley(), new Cemetery()
 			);
 
-			List<String> profaneCapitalMap = Arrays.asList(
-					"..++++++..+++...........................++++......+++.................+++.......",
-					"........+++++..............................+++++++.................+++++........",
-					"...........+++.......................C...............................+++++......",
-					"........................................................................++......",
-					".........................................................................+++....",
-					"............................+.................................C...........+++...",
-					".............................+++.......++++.....................................",
-					"........C....................++.......+......................++++...............",
-					".............................................................+++++++............",
-					"..................................###___###...................+++...............",
-					"..................................#_______#......................+++............",
-					"...........++.....................#_______#.......................+.............",
-					".........+++......................#_______#........................++...........",
-					"............+++...................####_####..........................+..........",
-					"..............+......................................................++.........",
-					"..............++.................................................++++++.........",
-					"............+++...................................................++++..........",
-					"+..................................................................++...........",
-					"++...+++................C........................................++++...........",
-					"+++......................................+++........................+.++........",
-					"++++.......++++.........................++.........................+....++......",
-					"#####___#####++++......................+..................C............+..+.....",
-					".............#.++......................+...................................+....",
-					"...+.....+...#+++...........................................................+...",
-					"...+.....+...#.+.....+++++...++.....+#___#++.................................++.",
-					".............#.++++++++++++++.+++.++##_._##++++...............................++");
-
-		List<String> anorLondoMap = Arrays.asList(
-					"......+++++......................#...........#..................................",
-					"...++++......................#####___________#####....+++.........+++++.........",
-					".+++...............+++.......#...................#..++++......C.....+++++++.....",
-					"..+++....C...........++......#####...........#####...+++..............+++.......",
-					".++................+++...........#...........#......++..................++......",
-					"+..............+++++++.......#####...........#####..............++..............",
-					"..................+++++++....#...................#.............+++++............",
-					"................+++..........#####___________#####...........+++...##.....##....",
-					".................................#...........#.............+++...##..#####..##..",
-					"....#_____#............######..###...........###..##__##....++.##.............##",
-					"...#.......#..........#......##..#...........#..##......#........##.........##..",
-					"...##.....##....######........#...#.........#...#........######....##.....##....",
-					"...#.......#...#......#............#.......#............#......#....#.....#.....",
-					"#####.....#####........######......#.......#......######........#####_____####..",
-					"#.................C.........................................C.................#.",
-					"#.............................................................................#.",
-					"####_______####........######......#.......#......######........#####_____####..",
-					"...............########......#####################......##____##..#.....#.......",
-					".+++.............++............####.........####...................##.....##....",
-					"++.............+++...........##.................##....+++++++....##.........##..",
-					".+++........#######........##...++.....+.....++...##....++++...##.............##",
-					"..+........#.......########............+............##....+++....##..#####..##..",
-					"...........#.................+++++...+++++...+++++..##...++........##.....##....",
-					"...........#.......########............+............##....++....................",
-					".....+......#######........##...++.....+.....++...##...............++...........",
-					"....+........................##.................##...+..............+++.........",
-					"..+++...............+++........#####.......#####...++.................++++......",
-					"...++......C......+++++...........##.......##....+++.........C.........++.......",
-					".....................++++.......###.........###....++.................+++.......",
-					"....................++.......#####################..+++..............++++++++...");
-			GameMap profaneCapital = new GameMap(groundFactory, profaneCapitalMap);
-			GameMap anorLondo = new GameMap(groundFactory, anorLondoMap);
-
 			// Create the maps
+			GameMap profaneCapital = new GameMap(groundFactory, Maps.PROFANE_CAPITAL);
+			GameMap anorLondo = new GameMap(groundFactory, Maps.ANOR_LONDO);
 			world.addGameMap(profaneCapital);
 			world.addGameMap(anorLondo);
 
-			// Spawn the player
-			Location playerSpawn = profaneCapital.at(38, 24);
-			Actor player = new Player("Unkindled (Player)", '@', 200, playerSpawn);
-			world.addPlayer(player, playerSpawn);
+			// Spawn player
+			spawnPlayer(profaneCapital.at(38, 24), world);
 
-			// Spawn profane capital bonfire
-			Location profaneCapitalBonfireLocation = profaneCapital.at(38, 11);
-			Bonfire profaneCapitalBonfire = new Bonfire(profaneCapitalBonfireLocation, "Profane Capital");
-			profaneCapitalBonfireLocation.setGround(profaneCapitalBonfire);
-
-			// Spawn anor londo bonfire
-			Location anorLondoBonfireLocation = anorLondo.at(39, 11);
-			Bonfire anorLondoBonfire = new Bonfire(anorLondoBonfireLocation, "Anor Londo Bonfire");
-			anorLondoBonfireLocation.setGround(anorLondoBonfire);
+			// Spawn bonfires
+			spawnBonfire(profaneCapital.at(38, 11), "Profane Capital Bonfire");
+			spawnBonfire(anorLondo.at(39, 11), "Anor Londo Bonfire");
 
 			// Place some skeletons around the map
-			profaneCapital.at(38, 18).addActor(new Skeleton("Skeleton", profaneCapital.at(38, 18)));
-			profaneCapital.at(38 + 15, 12 - 5).addActor(new Skeleton("Skeleton", profaneCapital.at(38 + 15, 12 - 5)));
-			profaneCapital.at(38 + 35, 12).addActor(new Skeleton("Skeleton", profaneCapital.at(38 + 35, 12)));
-			profaneCapital.at(38 - 20, 12 - 10).addActor(new Skeleton("Skeleton", profaneCapital.at(38 - 20, 12 - 10)));
-
+			// Randomly place skeletons on the map
+			spawnSkeleton(profaneCapital.at(38, 18), "Steve");
+			spawnSkeleton(profaneCapital.at(38 + 15, 12 - 5), "Skeleton");
+			spawnSkeleton(profaneCapital.at(38 + 35, 12), "Skeleton");
+			spawnSkeleton(profaneCapital.at(38 - 20, 12 - 10), "Skeleton");
 
 			// Place Yhorm the Giant/boss in the map + storm ruler
-			Location yhormSpawn = profaneCapital.at(6, 25);
-			yhormSpawn.addActor(new Yhorm(yhormSpawn));
-			StormRuler stormRuler = new StormRuler();
-			profaneCapital.at(6,20).addItem(stormRuler);
+			spawnYhorm(profaneCapital.at(6, 25));
+			spawnStormRuler(profaneCapital.at(6, 20));
 
-			Location chestLocation = profaneCapital.at(39,14);
-			Chest chest = new Chest(chestLocation.getGround(),chestLocation);
-			chestLocation.setGround(chest);
+			// Spawn fog door from profane capital to anor londo
+			spawnInterMapFogDoor(
+					profaneCapital.at(39, 25), anorLondo.at(39, 0),		// Location where fog door is placed
+					profaneCapital.at(39, 24), anorLondo.at(39, 1)		// Entry/Exits (vice versa)
+			);
 
+			// Spawn fog door into aldrich's chamber
+			spawnIntraMapFogDoor(anorLondo.at(39, 17), anorLondo.at(39, 16), anorLondo.at(39, 18));
 
-			// TODO spawn fog doors:
-			profaneCapital.at(39, 25).setGround(new FogDoor(profaneCapital.at(39, 24), anorLondo.at(39, 1)));
-			anorLondo.at(39, 0).setGround(new FogDoor(anorLondo.at(39, 1), profaneCapital.at(39, 24)));
+			// Spawn Aldrich
+			spawnAldrich(anorLondo.at(39, 27));
 
-			Location aldrichFogDoorLocation = anorLondo.at(39, 17);
-			FogDoor aldrichFogDoor = new FogDoor(anorLondo.at(39, 16), anorLondo.at(39, 18));
-			aldrichFogDoorLocation.setGround(aldrichFogDoor);
+			//Spawn Skeletons in Anor Londo
+			spawnSkeleton(anorLondo.at(7, 13), "Skeleton");
+			spawnSkeleton(anorLondo.at(71, 11), "Skeleton");
+			spawnSkeleton(anorLondo.at(71, 18), "Skeleton");
+			spawnSkeleton(anorLondo.at(26, 11), "Skeleton");
+			spawnSkeleton(anorLondo.at(54, 11), "Skeleton");
+			spawnSkeleton(anorLondo.at(17, 22), "Skeleton");
+			spawnSkeleton(anorLondo.at(66, 26), "Skeleton");
+			spawnSkeleton(anorLondo.at(58, 26), "Skeleton");
+			spawnSkeleton(anorLondo.at(26, 3), "Skeleton");
+			spawnSkeleton(anorLondo.at(9, 6), "Skeleton");
+			spawnSkeleton(anorLondo.at(55, 7), "Skeleton");
+			spawnSkeleton(anorLondo.at(72, 6), "Skeleton");
 
-			// TODO: Spawn Alrdich the Devourer in Anor Londo
-			Location aldrichSpawn = anorLondo.at(39, 27);
-			aldrichSpawn.addActor(new Aldrich(aldrichSpawn));
+			// Spawn chests randomly in profane capital and anor londo
+			spawnRandomChests(profaneCapital);
+			spawnRandomChests(anorLondo);
 
-			// TODO: Spawn Skeletons + cemeteries in Anor Londo:
-			//Second brackets don't do anything?
-			anorLondo.at(7, 13).addActor(new Skeleton("Skeleton", anorLondo.at(7, 13)));
-			anorLondo.at(71, 11).addActor(new Skeleton("Skeleton", anorLondo.at(71, 11)));
-			anorLondo.at(71, 18).addActor(new Skeleton("Skeleton", anorLondo.at(71, 18)));
-			anorLondo.at(26, 11).addActor(new Skeleton("Skeleton", anorLondo.at(26, 11)));
-			anorLondo.at(54, 11).addActor(new Skeleton("Skeleton", anorLondo.at(54, 11)));
-			anorLondo.at(17, 22).addActor(new Skeleton("Skeleton", anorLondo.at(17, 22)));
-			anorLondo.at(66, 26).addActor(new Skeleton("Skeleton", anorLondo.at(66, 26)));
-			anorLondo.at(58, 26).addActor(new Skeleton("Skeleton", anorLondo.at(58, 26)));
-			anorLondo.at(26, 3).addActor(new Skeleton("Skeleton", anorLondo.at(26, 3)));
-			anorLondo.at(9, 6).addActor(new Skeleton("Skeleton", anorLondo.at(9, 6)));
-			anorLondo.at(55, 7).addActor(new Skeleton("Skeleton", anorLondo.at(55, 7)));
-			anorLondo.at(72, 6).addActor(new Skeleton("Skeleton", anorLondo.at(72, 6)));
-
-			// TODO: spawn some chests
-
-
-			// TODO: figure out how to randomly spread chests in the map
-			// TODO: get a list of all locations where ground is dirt
-			// TODO: spawn X chests on different locations randomly
-			NumberRange profaneCapitalXRange = profaneCapital.getXRange();
-			NumberRange profaneCapitalYRange = profaneCapital.getYRange();
-
-			NumberRange anorLondoXRange = profaneCapital.getXRange();
-			NumberRange anorLondoYRange = profaneCapital.getYRange();
-
-			ArrayList<Location> dirtLocations = new ArrayList<>();
-			ArrayList<Location> dirtLocationss = new ArrayList<>();
-
-			Random r = new Random();
-			//Spawn Chests in Anor Londo
-			for (int x = anorLondoXRange.min(); x < anorLondoXRange.max(); x++){
-				for (int y = anorLondoYRange.min(); y < anorLondoYRange.max(); y++){
-					if (anorLondo.at(x,y).getGround() instanceof Dirt){
-						dirtLocationss.add(anorLondo.at(x,y));
-					}
-				}
-			}
-
-			for (int i = 0; i < dirtLocationss.size(); i++){
-				Location dirtLocation = dirtLocationss.get(i);
-				if (r.nextFloat() <= 0.005f){
-					Ground oldGround = dirtLocation.getGround();
-					dirtLocation.setGround(new Chest(oldGround,dirtLocation));
-				}
-			}
-
-
-			//Spawn chests in Profane Capital
-			for (int x = profaneCapitalXRange.min(); x < profaneCapitalXRange.max(); x++){
-				for (int y = profaneCapitalYRange.min(); y < profaneCapitalYRange.max(); y++){
-					if (profaneCapital.at(x,y).getGround() instanceof Dirt){
-						dirtLocations.add(profaneCapital.at(x,y));
-					}
-				}
-			}
-
-			for (int i = 0; i < dirtLocations.size(); i++){
-				Location dirtLocation = dirtLocations.get(i);
-				if (r.nextFloat() <= 0.005f){
-					Ground oldGround = dirtLocation.getGround();
-					dirtLocation.setGround(new Chest(oldGround,dirtLocation));
-				}
-			}
-
-		world.run();
-
+			// Run the game
+			world.run();
 	}
 }
